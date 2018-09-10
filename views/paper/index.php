8f752cb69813528e47188a3918714083ce698d43
Manisha Solanki <solankimanisha777@gmail.com>
2018-09-10 20:48:38 +0530

    query

--- views/paper/index.php
+++ views/paper/index.php
@@ -12,11 +12,11 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="paper-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 style="display: inline-block"><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Paper', ['create'], ['class' => 'btn btn-success']) ?>
+    <p style="display: inline-block">
+        <?= Html::a('+ Add Paper', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
